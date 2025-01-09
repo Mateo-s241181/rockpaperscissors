5ba17ce3df0840e956b6a7df8d37b74ca54d4ab1
Reiner Huechting <dev@reinerhuechting.de>
2025-01-09 21:37:48 +0100

    Remove hints.

--- values/values.go
+++ values/values.go
@@ -10,17 +10,12 @@ const (
 
 // String liefert den Wert als String.
 func (v Value) String() string {
-	// Hinweis: Verwenden Sie einen switch-case Block um den Wert v in einen String umzuwandeln.
-
 	// TODO
 	return ""
 }
 
 // Beats gibt an, ob der Wert v den Wert w schlägt.
 func (v Value) Beats(w Value) bool {
-	// Hinweis: Verwenden Sie einen if-else- oder switch-Block um zu prüfen, ob der Wert v den Wert w schlägt.
-	//          Alternativ können Sie auch die Werte in Integer umwandeln und rechnerisch prüfen.
-
 	// TODO
 	return false
 }
